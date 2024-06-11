b0549c403772f403eb0c1c8e81f99e8d103caa5f
JustinRafaelYahya <justin.rafael0@gmail.com>
2024-06-11 20:57:53 +0700

    third commit

--- src/app/@overview/default.jsx
+++ src/app/@overview/default.jsx
@@ -1,12 +1,6 @@
 "use client";
 
-import { useState } from "react";
 import React from "react";
-import { LiaShippingFastSolid } from "react-icons/lia";
-import { FaRegStar } from "react-icons/fa";
-import { HiArrowsPointingIn } from "react-icons/hi2";
-import { VscTools } from "react-icons/vsc";
-import OverviewCards from "@/components/OverviewCards";
 
 export default function Default() {
   return (
